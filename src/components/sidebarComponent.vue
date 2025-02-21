d857243cb49fcfddbb27c5c10fcffd5eb7b91d34
Edison-E <enrikvc2002@gmail.com>
2025-02-21 23:38:27 +0100

    New style in navbar

--- src/components/sidebarComponent.vue
+++ src/components/sidebarComponent.vue
@@ -4,40 +4,32 @@ export default {
 }
 </script>
 <template>
-    <nav class="navigation">
-        <ul class="nav nav-tabs">
-            <li class="nav-item">
-                <a class="nav-link" aria-current="page" href="#">☽</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="https://www.linkedin.com/in/edisonenriquedev/">linkedIn</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Currículum</a>
-            </li>
-        </ul>
-    </nav>
+
+    <ul class="nav justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link" href="#">☽</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href=" https://www.linkedin.com/in/edisonenriquedev/">LinkedIn</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Currículum</a>
+        </li>
+    </ul>
+
 </template>
 <style scoped>
-.navigation {
-    margin-left: 100px;
-    margin-bottom: 40px;
-    width: 80%;
+.nav {
+    width: 50%;
+    margin: auto;
+    text-align: center;
 }
 
 .nav-link {
-    color: orange
+    color: orange;
 }
 
 .nav-link:hover {
-    background-color: rgb(52, 49, 49);
     color: white;
-}
-
-.nav-item {
-    border: 1px solid white;
-    border-top-left-radius: 5px;
-    border-top-right-radius: 5px;
-    margin-right: 20px;
 }
 </style>
