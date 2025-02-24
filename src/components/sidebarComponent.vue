9fd1a33d290d57f420d201b8ca543d82f4e3129a
Edison-E <enrikvc2002@gmail.com>
2025-02-24 23:44:48 +0100

    New icons in navbar

--- src/components/sidebarComponent.vue
+++ src/components/sidebarComponent.vue
@@ -7,18 +7,36 @@ export default {
 
     <ul class="nav justify-content-center">
         <li class="nav-item">
-            <a class="nav-link" href="#">☽</a>
+            <a class="nav-link" href="#">
+                <img  src="@/assets/svg/lightbulb.svg" alt="light" id="light">
+            </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href=" https://www.linkedin.com/in/edisonenriquedev/">LinkedIn</a>
+            <a class="nav-link" href=" https://www.linkedin.com/in/edisonenriquedev/">
+                <img src="@/assets/svg/linkedin.svg" alt="linkedin" id="linkedin">
+            </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Currículum</a>
+            <a class="nav-link" href="#">
+                <img src="@/assets/svg/information.svg" alt="curriculum" id="curriculum">
+            </a>
         </li>
     </ul>
 
 </template>
 <style scoped>
+#light {
+    width: 11%;
+}
+
+#linkedin {
+    width: 6%;
+}
+
+#curriculum {
+    width: 11%;
+}
+
 .nav {
     width: 50%;
     margin: auto;
