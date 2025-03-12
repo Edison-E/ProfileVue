21b874b2ee9073eec3827e170847d55dbad92e7f
Edison-Enrique Crespo-Vasquez <edison.crespo.ext@inetum.com>
2025-03-12 13:38:30 +0100

    New style in skillComponent

--- src/components/skillComponent.vue
+++ src/components/skillComponent.vue
@@ -1,4 +1,6 @@
 <script>
+import xmlTechImg from '@/assets/image/xmlTech.png';
+
 export default {
     name: "skillComponent",
     data() {
@@ -14,33 +16,40 @@ export default {
             },
             skill: {
                 "FrontEnd": {
+                    'img': require('@/assets/image/front.png'),
                     'Javascript': 100,
                     'Html': 100,
-                    'Css': 100
+                    'Css': 100,
+                    'Vue.js': 100,
+                    'JQuery': 100,
+                    'Bootstrap':70
                 },
                 "BackEnd": {
+                    'img': require('@/assets/image/back.png'),
                     'C#': 100,
                     'Visual Basic': 100,
                     'Java': 100,
-                    'PHP': 70
-                },
-                "DataBase": {
+                    'PHP': 70,
                     'Sql': 100,
-                    'Pl/Sql': 100
+                    'Pl/Sql': 100,
+                    '.NET Framework':100,
+                    'Laravel' : 70
                 }
             },
-            FrameworkTechnologies: {
-                "Framework": ['Vue', 'JQUERY', 'Bootstrap', '.NET Framework', 'Laravel'],
-                "Technologies": ['XML', 'XSD', 'DTD', 'XSLT', 'XPATH']
+            Technologies: {
+                "XMLTech": {
+                    'img': xmlTechImg,
+                    'items':  ['XML', 'XSD', 'DTD', 'XSLT', 'XPATH']
+                }
             },
-            Tools: ["Oracle", "Visual Studio", "Visual Studio Code", "Neatbeans", "Apache", "GitHub", "BitBucket"]
+            Tools: ["Oracle", "Visual Studio", "Visual Studio Code", "Neatbeans", "Apache", "GitHub", "BitBucket"],
         }
     },
     methods: {
         visibleLenguaje(typeLenguaje) {
             this.isVisible[typeLenguaje] = !this.isVisible[typeLenguaje]
         },
-        visibleFrameworkTechnologies(category) {
+        visibleTechnologies(category) {
             this.isVisible[category] = !this.isVisible[category]
         },
         visibleTools(typeTool) {
@@ -54,13 +63,14 @@ export default {
         <!--Lenguaje-->
         <div class="row container-lenguaje">
             <div v-for="(skills, category) in skill" :key="category" class="col-md-4">
-                <div class="skill-card" @click="visibleLenguaje(category)">
-                    {{ category }}
+                <div class="skill-card">
+                   <img  id="img-card" :src="skills.img" alt="Camino"  @click="visibleLenguaje(category)">
+                   <span id="title-card"> {{ category }}</span>
                 </div>
                 <div v-show="isVisible[category]" class="details">
                     <div v-for="(percentage, language) in skills" :key="language" class="skill-item">
-                        <span class="language">{{ language }}</span>
-                        <div class="progress-container">
+                        <span class="language" v-if="language !== 'img'">{{ language }}</span>
+                        <div class="progress-container"  v-if="language !== 'img'">
                             <div class="progress" role="progressbar" :aria-valuenow="percentage" aria-valuemin="0"
                                 aria-valuemax="100">
                                 <div class="progress-bar" :class="percentage === 100 ? 'bg-success' : 'bg-warning'"
@@ -74,14 +84,15 @@ export default {
         </div>
 
         <!--Framework Technologies and Tools-->
-        <div class="row mt-3 container-FrameworkTechnologies">
+        <div class="row mt-3 container-Technologies">
             <!--Framework Technologies-->
-            <div v-for="(values, category) in FrameworkTechnologies" :key="category" class="col-md-6">
-                <div class="skill-card" @click="visibleFrameworkTechnologies(category)">
-                    {{ category }}
+            <div v-for="(values, category) in Technologies" :key="category" class="col-md-6">
+                <div class="skill-card">
+                    <img :src="values.img" alt="icono" id="img-card"  @click="visibleTechnologies(category)">
+                    <span id="title-card">{{ category }}</span>
                 </div>
-                <div v-show="isVisible[category]" class="details">
-                    <div v-for="(value, index) in values" :key="index">
+                <div v-show="isVisible[category]" class="details-other">
+                    <div v-for="(value, index) in values.items" :key="index">
                         {{ value }}
                     </div>
                 </div>
@@ -89,10 +100,12 @@ export default {
 
             <!--Tools-->
             <div class="col-md-4">
-                <div class="skill-card" @click="visibleTools('Tool')">
-                    Tools
+                <div class="skill-card" >
+                     <img src="../assets/image/tool.png" alt="icono" id="img-card" @click="visibleTools('Tool')">
+                     <span id="title-card">Tools</span>
+                    
                 </div>
-                <div v-show="isVisible['Tool']" class="details">
+                <div v-show="isVisible['Tool']" class="details-other">
                     <div v-for="(tool, index) in Tools" :key="index">
                         {{ tool }}
                     </div>
@@ -109,9 +122,35 @@ export default {
 }
 
 .skill-card {
-    background: linear-gradient(to bottom, rgb(45, 50, 62), rgb(18, 20, 28));
-    color: orange;
+    width: 100%;
+    color: #ffa500;
     margin-bottom: 10px;
+}
+
+#title-card {
+    margin-left: 5%;
+}
+
+#img-card{
+    width: 40px;
+    height: 40px;
+}
+
+#img-card:hover {
+    transform: scale(1.2);
+    filter: drop-shadow(0px 0px 10px rgba(255, 165, 0, 0.8));
+}
+
+.details-other {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 10px;
+    margin-top: 10px;
+}
+
+.details {
+    margin-left: 35%;
 }
 
 .skill-item {
@@ -134,10 +173,6 @@ export default {
 
 .progress {
     height: 10px;
-}
-
-.skill-card:hover {
-    background: linear-gradient(to bottom, rgb(61, 67, 82), rgb(31, 34, 46));
 }
 
 .col-md-4,
