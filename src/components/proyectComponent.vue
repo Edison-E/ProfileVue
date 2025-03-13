8b7466f8437684b59539021e5dcf545af48a091b
Edison-E <enrikvc2002@gmail.com>
2025-03-13 21:34:46 +0100

    new style in proyect for card-proyect

--- src/components/proyectComponent.vue
+++ src/components/proyectComponent.vue
@@ -12,17 +12,19 @@ export default {
 }
 </script>
 <template>
-    <div class="container-sm border-bottom proyectContainer">
+    <div class="container-sm proyectContainer">
         <h1>Proyects</h1>
 
-        <div v-for="(value, proyect) in proyect" :key="proyect" class="card border-light">
-            <div class="card-header border-light">{{ proyect }}</div>
-            <div class="card-body">
+        <div v-for="(value, proyect) in proyect" :key="proyect" class="card-proyect">
+            <div class="card-title">{{ proyect }}</div>
+            <div class="card-details">
                 <p class="card-text">{{ value[0] }}</p>
-                <a :href="value[2]" class="btn btn-primary">Go repository</a>
+                <a :href="value[2]" class="">Go repository</a>
+                <p class="card-update"> {{ value[1] }}</p>
             </div>
-            <div class="card-footer border-light"> {{ value[1] }}</div>
+            <br>
         </div>
+
     </div>
 
 </template>
@@ -34,14 +36,25 @@ export default {
     margin-bottom: 20px;
 }
 
-.card {
-    margin-top: 3%;
-    width: 50%;
-    background: linear-gradient(to bottom, rgb(45, 50, 62), rgb(18, 20, 28));
-    color: white;
+.card-proyect{
+    margin-top: 5%;
+    padding: 2%;
+    width: 40%;
+    height: 10%;
+    background-color: rgba(255, 255, 255, 0.05);
+    transition: all 0.3s ease-in-out;
 }
 
-.card-footer {
-    color: white;
+.card-title {
+    font-weight: bold;
 }
+
+.card-proyect:hover {
+    background: linear-gradient(135deg, rgba(92, 91, 91, 0.8), rgba(80, 80, 80, 0.5));
+    box-shadow: 0 5px 5px rgba(255, 255, 255, 0.2);
+    transform: scale(1.05);
+    color: #fff;
+}
+
+
 </style>
